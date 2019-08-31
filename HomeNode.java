c818ea6a02384f95cb5fa79624262a8d535025d8
zacrogers <38870655+zacrogers@users.noreply.github.com>
2019-09-01 11:49:11 +1200

    Create HomeNode.java

---
+++ HomeNode.java
@@ -0,0 +1,27 @@
+
+import net.dongliu.requests.body.*;
+
+public class HomeNode{
+  private int    numRelays;
+  private String ipAddr;
+  private String name;
+  
+  public HomeNode(String name, String ipAddr int numRelays){
+    this.name      = name;
+    this.ipAddr    = ipAddr;
+    this.numRelays = numRelays;
+  }
+  
+  public void setRelay(int relay, boolean state){
+    if((relay < numRelays) && (relay > 0)){
+        requests.get(String.format("http://%s/re%d_%s", ipAddr, relay, state))).send();
+    }
+  }
+
+  public String getTemp(){
+    String temp = "N/A";
+    
+    return temp;
+  }
+
+}
